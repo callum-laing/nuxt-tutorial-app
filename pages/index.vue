80b9be914d6140c9f83d7dcea731f4a3827ab5c3
Callum Laing <callum.laing91@hotmail.co.uk>
2024-07-30 22:50:42 +0100

    initial commit & app built up to fetching data from api

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur
+      saepe, recusandae nam optio asperiores, ratione aspernatur ea sapiente
+      eius eveniet voluptatibus dolor. Voluptas nemo rem necessitatibus quidem
+      delectus fugiat ratione!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  font-size: 5rem;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "products",
+});
+
+// fetch the products from the fakestore API
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
+
+<style scoped>
+h2 {
+  font-size: 5rem;
+}
+
+.router-link-active {
+  color: red;
+}
+</style>
